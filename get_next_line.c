ab8f25fc79908bdf055294beacd004c6cf2a0906
Theo Mongellaz <theo.mongellaz@gmail.com>
2022-01-11 17:38:06 +0100

    added more bugs, now they look like something I can work on

--- get_next_line.c
+++ get_next_line.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/05 17:56:24 by tmongell          #+#    #+#             */
-/*   Updated: 2022/01/10 16:25:24 by tmongell         ###   ########.fr       */
+/*   Created: 2022/01/10 16:47:19 by tmongell          #+#    #+#             */
+/*   Updated: 2022/01/11 15:19:39 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,70 +15,72 @@
 #ifndef BUFFER_SIZE
 # define BUFFER_SIZE 42
 #endif
-//check that the content of a string don't contain a \n or \0.
-int	ft_no_newline(char *str)
+
+int	line_not_finished(char *buff)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0' && str[i] != '\n')
+	while (i <= BUFFER_SIZE)
+	{
+		if (buff[i] == '\n' || buff[i] == '\0')
+			return (0);
 		i ++;
-//	printf("debug(ft_no_newline,L26) : i = %d\n", i);//=========================DEBUG
-	if (i == BUFFER_SIZE || i == 0)
-		return (1);
-	else
-		return (0);
+	}
+	return (1);
 }
 
-//add the content of the buffer to the return value
-char	*ft_savebuff(char *line, char *buff)
+//note : I should free old_line befor the return, but malloc won't let me ;(
+char	*ft_save_buff(char *line, char *buff)
 {
 	char	*old_line;
 
 	old_line = line;
 	line = ft_strjoin(line, buff);
-	free(old_line);
+//	free(old_line);
 	return (line);
 }
 
-// once the buffer contain the end of line/file, coppy the end of the line to
-// the return value then move the rest of the buffer to buff[0]
-char	*ft_finish_line(char *line, char *buff)
+char	*ft_finish_line(char *buff, char *line, char *leftover)
 {
-	char	end_line[BUFFER_SIZE + 1];
+	char	end_of_line[BUFFER_SIZE + 1];
+	char	*true_line;
 	int		i;
-
+	int		j;
 	i = 0;
 	while (buff[i] != '\n' && buff[i] != '\0')
 	{
-		end_line[i] = buff[i];
+		end_of_line[i] = buff[i];
 		i ++;
 	}
-	end_line[i] = buff[i];
-	line = ft_savebuff(line, end_line);
-//	if (buff[i])
-	ft_strlcpy(buff, buff + i + 1, BUFFER_SIZE);
-	buff[i + 1] = '\0';
-	return (line);
+	end_of_line[i] = buff[i];
+	true_line = ft_strjoin(line, end_of_line);
+	j = 0;
+	while (i < BUFFER_SIZE && buff[i])
+	{
+		leftover[j] = buff[i];
+		i ++;
+		j ++;
+	}
+	leftover[j] = '\0';
+//	free(line);
+	return (true_line);
 }
 
-//the main function
 char	*get_next_line(int fd)
 {
-	static char	buff[BUFFER_SIZE + 1];
+	static char	leftover[BUFFER_SIZE + 1];
+	char		buff[BUFFER_SIZE + 1];
 	char		*line;
 
-	line = malloc(1);
-	line[0] = '\0';
-	buff[BUFFER_SIZE] = '\0';
-//	printf("debug(get_next_line,L73) : gnl initialized\n");//===================DEBUG
-	while (ft_no_newline(buff))
+	line = ft_strdup(leftover);
+	buff[BUFFER_SIZE] = '1';
+	while (line_not_finished(buff) || buff[BUFFER_SIZE])
 	{
-		line = ft_savebuff(line, buff);
-//		printf("debug(get_next_line,L77) : line = %s\n", line);//===============DEBUG
+		buff[BUFFER_SIZE] = '\0';
+		line = ft_save_buff(line, buff);
 		read(fd, buff, BUFFER_SIZE);
-//		printf("debug(get_next_line,L79) : new buff = %s\n", buff);//===========DEBUG
 	}
-	line = ft_finish_line(line, buff);
+	line = ft_finish_line(buff, line, leftover);
 	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 17:48:26 by tmongell          #+#    #+#             */
-/*   Updated: 2022/01/07 23:03:26 by tmongell         ###   ########.fr       */
+/*   Updated: 2022/01/10 21:16:22 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,13 @@
 
 //function from main file
 char	*get_next_line(int fd);
-int		ft_no_newline(char *str);
-char	*ft_savebuff(char *line, char *buff);
+int		line_mot_finished(char *str);
+char	*ft_save_buff(char *line, char *buff);
 
 //function from utils
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 #endif
