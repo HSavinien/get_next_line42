3fd4568d6015245cc7f9bcee9d880d538c609d1b
Theo Mongellaz <theo.mongellaz@gmail.com>
2022-02-01 17:23:29 +0100

    put program to norminette

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tmongell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 04:07:17 by tmongell          #+#    #+#             */
-/*   Updated: 2022/02/01 17:00:33 by tmongell         ###   ########.fr       */
+/*   Updated: 2022/02/01 17:22:06 by tmongell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,7 @@ char	*save_buf(char *base_str, char *buf)
 	char	*new_str;
 	int		base_size;
 	int		buf_size;
-	
+
 	base_size = ft_strlen(base_str);
 	buf_size = 0;
 	while (buf[buf_size] && buf[buf_size] != '\n')
@@ -65,8 +65,8 @@ char	*save_buf(char *base_str, char *buf)
 	if (buf[buf_size] == '\n')
 		buf_size ++;
 	new_str = malloc(base_size + buf_size + 1);
-		if(!new_str)
-			return (NULL);
+	if (!new_str)
+		return (NULL);
 	ft_strlcpy(new_str, base_str, base_size + 1);
 	ft_strlcpy(new_str + base_size, buf, buf_size + 1);
 	free(base_str);
@@ -77,13 +77,13 @@ char	*save_leftover(char *str)
 {
 	char	*leftover;
 	int		i;
-	
+
 	i = 0;
 	while (str[i] != '\0' && str[i] != '\n')
-		 i++;
+		i++;
 	if (!str[i])
 		return (ft_strdup(""));
 	str[i ++] = '\0';
 	leftover = ft_strdup(str + i);
-	return (leftover);	
+	return (leftover);
 }
